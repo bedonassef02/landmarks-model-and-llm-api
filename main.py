a21cb51db2e066e1fd1b9e7b203d67d0df85945b
Bedo <bedonassef71@gmail.com>
2024-05-09 13:48:10 +0300

    test llm

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from src.routes.chat_route import chat_route
-from src.routes.predict_route import predict_route
+# from src.routes.predict_route import predict_route
 # import src.utils.test
 # import src.utils.combined_faiss_index
 
 app = Flask(__name__)
 
 # Registering routes
-app.register_blueprint(predict_route)
+# app.register_blueprint(predict_route)
 app.register_blueprint(chat_route)
 
 if __name__ == '__main__':
